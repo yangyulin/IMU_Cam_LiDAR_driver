697752c4133b1f6d05c07a5544c027d517b72c05
linde <yuyang@udel.edu>
2017-06-19 05:20:54 -0400

    add xsens, zed and velodyne driver

---
+++ zed_cpu_ros/README.md
@@ -0,0 +1,79 @@
+zed_cpu_ros
+===========
+A simple zed camera driver which only use CPU and only publish left and right raw images and its camera info.
+
+# Useage:
+1. git the packge into your working space
+
+    ```
+    cd catkin_ws/src
+    git clone https://github.com/transcendrobotics/zed_cpu_ros
+    cd ..
+    catkin_make
+    ```
+2. Get your calibration files:
+    You can get your calibration files from zed or do a calibration your self by using ROS camera calibration package.
+    
+    (1). From zed:
+    
+    Find your zed calibration files in
+    ```
+    cd /usr/local/zed/settings
+    ```
+    or download from:
+    http://calib.stereolabs.com/?SN=XXXX
+
+    Note: XXXX is your last four digit S/N of your camera, make sure to change it!!
+
+    put the .conf file into zed_cpu_ros/config folder
+
+    update launch file configuration file name in zed_cpu_ros.launch into your SNXXXX.conf
+    ```
+    roscd zed_cpu_ros/launch
+    gedit zed_cpu_ros.launch
+    ```
+    change XXXX into the .conf file you have, for example 1010
+    ```
+    <arg name="config_file_location" default="$(find zed_cpu_ros)/config/SN1010.conf"/>
+    ```
+
+    (2). Do a calibration yourself:
+    
+    This option is suggested. Reference: http://wiki.ros.org/camera_calibration
+    ```
+    roslaunch zed_cpu_ros camera_calibration.launch
+    ```
+    After calibration:
+    Find the left.yaml and right.yaml in the tar file and put them into the zed_cpu_ros/config folder.
+    The calibration file will be loaded if you turn <load_zed_config> off in the launch file.
+
+3. launch the code
+    ```
+    roslaunch zed_cpu_ros zed_cpu_ros.launch
+    ```
+## Launch file parameters
+
+ Parameter                    |           Description                                       |              Value          
+------------------------------|-------------------------------------------------------------|-------------------------           
+ resolution                   | ZED Camera resolution                                       | '0': HD2K                   
+ _                            | _                                                           | '1': HD1080                 
+ _                            | _                                                           | '2': HD720                  
+ _                            | _                                                           | '3': VGA                                    
+ frame_rate                   | Rate at which images are published                          | int                                             
+ left_frame_id                | Left Frame ID                                               | string        
+ right_frame_id               | Right Frame ID                                              | string        
+ load_zed_config              | Whether to use ZED calibration file                         | bool        
+ config_file_location         | The location of ZED calibration file                        | string        
+ show_image                   | Whether to use opencv show image                            | bool        
+
+# TODO:
+
+1. add the launch file for stereo_proc.
+2. add the nodelet functionality.
+
+# Transcend Robotics:
+Patented articulated traction control ARTI technology for stair climbing and obstacle traversal without complex software or controls
+http://transcendrobotics.com/
+
+# Authour:
+Di Zeng 
---
+++ zed_cpu_ros/src/zed_cpu_ros.cpp
@@ -0,0 +1,386 @@
+#include <stdio.h>
+#include <string>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/Image.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/distortion_models.h>
+#include <image_transport/image_transport.h>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/ini_parser.hpp>
+#include <camera_info_manager/camera_info_manager.h>
+
+#define WIDTH_ID 3
+#define HEIGHT_ID 4
+#define FPS_ID 5
+
+namespace arti {
+
+
+class StereoCamera
+{
+
+public:
+
+	/**
+	 * @brief      { stereo camera driver }
+	 *
+	 * @param[in]  resolution  The resolution
+	 * @param[in]  frame_rate  The frame rate
+	 */
+	StereoCamera(int device_id, int resolution, double frame_rate): frame_rate_(30.0) {
+
+		camera_ = new cv::VideoCapture(device_id);
+		cv::Mat raw;
+		cv::Mat left_image;
+		cv::Mat right_image;
+		setResolution(resolution);
+		// // this function doesn't work very well in current Opencv 2.4, so, just use ROS to control frame rate.
+		// setFrameRate(frame_rate);
+
+		std::cout << "Stereo Camera Set Resolution: " << camera_->get(WIDTH_ID) << "x" << camera_->get(HEIGHT_ID) << std::endl;
+		// std::cout << "Stereo Camera Set Frame Rate: " << camera_->get(FPS_ID) << std::endl;
+	}
+
+	~StereoCamera() {
+		// std::cout << "Destroy the pointer" << std::endl;
+		delete camera_;
+	}
+
+	/**
+	 * @brief      Sets the resolution.
+	 *
+	 * @param[in]  type  The type
+	 */
+	void setResolution(int type) {
+
+		if (type == 0) { width_ = 4416; height_ = 1242;} // 2K
+		if (type == 1) { width_ = 3840; height_ = 1080;} // FHD
+		if (type == 2) { width_ = 2560; height_ = 720;}  // HD
+		if (type == 3) { width_ = 1344; height_ = 376;}  // VGA
+
+		camera_->set(WIDTH_ID, width_);
+		camera_->set(HEIGHT_ID, height_);
+
+		// make sure that the number set are right from the hardware
+		width_ = camera_->get(WIDTH_ID);
+		height_ = camera_->get(HEIGHT_ID);
+
+	}
+
+	/**
+	 * @brief      Sets the frame rate.
+	 *
+	 * @param[in]  frame_rate  The frame rate
+	 */
+	void setFrameRate(double frame_rate) {
+		camera_->set(FPS_ID, frame_rate);
+		frame_rate_ = camera_->get(FPS_ID);
+	}
+
+	/**
+	 * @brief      Gets the images.
+	 *
+	 * @param      left_image   The left image
+	 * @param      right_image  The right image
+	 *
+	 * @return     The images.
+	 */
+	bool getImages(cv::Mat& left_image, cv::Mat& right_image) {
+		cv::Mat raw;
+		if (camera_->grab()) {
+			camera_->retrieve(raw);
+			cv::Rect left_rect(0, 0, width_ / 2, height_);
+			cv::Rect right_rect(width_ / 2, 0, width_ / 2, height_);
+			left_image = raw(left_rect);
+			right_image = raw(right_rect);
+			cv::waitKey(10);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+private:
+	cv::VideoCapture* camera_;
+	int width_;
+	int height_;
+	double frame_rate_;
+	bool cv_three_;
+};
+
+/**
+ * @brief       the camera ros warpper class
+ */
+class ZedCameraROS {
+public:
+
+	/**
+	 * @brief      { function_description }
+	 *
+	 * @param[in]  resolution  The resolution
+	 * @param[in]  frame_rate  The frame rate
+	 */
+	ZedCameraROS() {
+		ros::NodeHandle nh;
+		ros::NodeHandle private_nh("~");
+		// get ros param
+		private_nh.param("device_id", device_id_, 1);
+		private_nh.param("resolution", resolution_, 1);
+		private_nh.param("frame_rate", frame_rate_, 30.0);
+		private_nh.param("config_file_location", config_file_location_, std::string("~/SN11819.conf"));
+		private_nh.param("left_frame_id", left_frame_id_, std::string("left_camera"));
+		private_nh.param("right_frame_id", right_frame_id_, std::string("right_camera"));
+		private_nh.param("show_image", show_image_, false);
+		private_nh.param("load_zed_config", load_zed_config_, true);
+
+		ROS_INFO("Try to initialize the camera");
+		StereoCamera zed(device_id_, resolution_, frame_rate_);
+		ROS_INFO("Initialized the camera");
+
+		// setup publisher stuff
+		image_transport::ImageTransport it(nh);
+		image_transport::Publisher left_image_pub = it.advertise("left/image_raw", 1);
+		image_transport::Publisher right_image_pub = it.advertise("right/image_raw", 1);
+
+		ros::Publisher left_cam_info_pub = nh.advertise<sensor_msgs::CameraInfo>("left/camera_info", 1);
+		ros::Publisher right_cam_info_pub = nh.advertise<sensor_msgs::CameraInfo>("right/camera_info", 1);
+
+		sensor_msgs::CameraInfo left_info, right_info;
+
+
+		ROS_INFO("Try load camera calibration files");
+		if (load_zed_config_) {
+			ROS_INFO("Loading from zed calibration files");
+			// get camera info from zed
+			try {
+				getZedCameraInfo(config_file_location_, resolution_, left_info, right_info);
+			}
+			catch (std::runtime_error& e) {
+				ROS_INFO("Can't load camera info");
+				throw e;
+			}
+		} else {
+			ROS_INFO("Loading from ROS calibration files");
+			// get config from the left, right.yaml in config
+			camera_info_manager::CameraInfoManager info_manager(nh);
+			info_manager.setCameraName("zed/left");
+			info_manager.loadCameraInfo( "package://zed_cpu_ros/config/left.yaml");
+			left_info = info_manager.getCameraInfo();
+
+			info_manager.setCameraName("zed/right");
+			info_manager.loadCameraInfo( "package://zed_cpu_ros/config/right.yaml");
+			right_info = info_manager.getCameraInfo();
+
+			left_info.header.frame_id = left_frame_id_;
+			right_info.header.frame_id = right_frame_id_;
+		}
+
+		// std::cout << left_info << std::endl;
+		// std::cout << right_info << std::endl;
+
+		ROS_INFO("Got camera calibration files");
+		// loop to publish images;
+		cv::Mat left_image, right_image;
+		ros::Rate r(frame_rate_);
+
+		while (nh.ok()) {
+			ros::Time now = ros::Time::now();
+			if (!zed.getImages(left_image, right_image)) {
+				ROS_INFO_ONCE("Can't find camera");
+			} else {
+				ROS_INFO_ONCE("Success, found camera");
+			}
+			if (show_image_) {
+				cv::imshow("left", left_image);
+				cv::imshow("right", right_image);
+			}
+
+			if (left_image_pub.getNumSubscribers() > 0) {
+				publishImage(left_image, left_image_pub, "left_frame", now);
+			}
+			if (right_image_pub.getNumSubscribers() > 0) {
+				publishImage(right_image, right_image_pub, "right_frame", now);
+			}
+			if (left_cam_info_pub.getNumSubscribers() > 0) {
+				publishCamInfo(left_cam_info_pub, left_info, now);
+			}
+			if (right_cam_info_pub.getNumSubscribers() > 0) {
+				publishCamInfo(right_cam_info_pub, right_info, now);
+			}
+			r.sleep();
+			// since the frame rate was set inside the camera, no need to do a ros sleep
+		}
+	}
+
+	/**
+	 * @brief      Gets the camera information From Zed config.
+	 *
+	 * @param[in]  config_file         The configuration file
+	 * @param[in]  resolution          The resolution
+	 * @param[in]  left_cam_info_msg   The left camera information message
+	 * @param[in]  right_cam_info_msg  The right camera information message
+	 */
+	void getZedCameraInfo(std::string config_file, int resolution, sensor_msgs::CameraInfo& left_info, sensor_msgs::CameraInfo& right_info) {
+		boost::property_tree::ptree pt;
+		boost::property_tree::ini_parser::read_ini(config_file, pt);
+		std::string left_str = "LEFT_CAM_";
+		std::string right_str = "RIGHT_CAM_";
+		std::string reso_str = "";
+
+		switch (resolution) {
+		case 0: reso_str = "2K"; break;
+		case 1: reso_str = "FHD"; break;
+		case 2: reso_str = "HD"; break;
+		case 3: reso_str = "VGA"; break;
+		}
+		// left value
+		double l_cx = pt.get<double>(left_str + reso_str + ".cx");
+		double l_cy = pt.get<double>(left_str + reso_str + ".cy");
+		double l_fx = pt.get<double>(left_str + reso_str + ".fx");
+		double l_fy = pt.get<double>(left_str + reso_str + ".fy");
+		double l_k1 = pt.get<double>(left_str + reso_str + ".k1");
+		double l_k2 = pt.get<double>(left_str + reso_str + ".k2");
+		// right value
+		double r_cx = pt.get<double>(right_str + reso_str + ".cx");
+		double r_cy = pt.get<double>(right_str + reso_str + ".cy");
+		double r_fx = pt.get<double>(right_str + reso_str + ".fx");
+		double r_fy = pt.get<double>(right_str + reso_str + ".fy");
+		double r_k1 = pt.get<double>(right_str + reso_str + ".k1");
+		double r_k2 = pt.get<double>(right_str + reso_str + ".k2");
+		// conver mm to m
+		double baseline = pt.get<double>("STEREO.Baseline") * 0.001;
+		// get Rx and Rz
+		double rx = pt.get<double>("STEREO.RX_"+reso_str);
+		double rz = pt.get<double>("STEREO.RZ_"+reso_str);
+		double ry = pt.get<double>("STEREO.CV_"+reso_str);
+		// assume zeros, maybe not right
+		double p1 = 0, p2 = 0, k3 = 0;
+
+		left_info.distortion_model = sensor_msgs::distortion_models::PLUMB_BOB;
+		right_info.distortion_model = sensor_msgs::distortion_models::PLUMB_BOB;
+
+		// distortion parameters
+		// For "plumb_bob", the 5 parameters are: (k1, k2, t1, t2, k3).
+		left_info.D.resize(5);
+		left_info.D[0] = l_k1;
+		left_info.D[1] = l_k2;
+		left_info.D[2] = k3;
+		left_info.D[3] = p1;
+		left_info.D[4] = p2;
+
+		right_info.D.resize(5);
+		right_info.D[0] = r_k1;
+		right_info.D[1] = r_k2;
+		right_info.D[2] = k3;
+		right_info.D[3] = p1;
+		right_info.D[4] = p2;
+
+		// Intrinsic camera matrix
+		// 	   [fx  0 cx]
+		// K = [ 0 fy cy]
+		// 	   [ 0  0  1]
+		left_info.K.fill(0.0);
+		left_info.K[0] = l_fx;
+		left_info.K[2] = l_cx;
+		left_info.K[4] = l_fy;
+		left_info.K[5] = l_cy;
+		left_info.K[8] = 1.0;
+
+		right_info.K.fill(0.0);
+		right_info.K[0] = r_fx;
+		right_info.K[2] = r_cx;
+		right_info.K[4] = r_fy;
+		right_info.K[5] = r_cy;
+		right_info.K[8] = 1.0;
+
+		// rectification matrix
+		// Rl = R_rect, R_r = R * R_rect
+		// since R is identity, Rl = Rr;
+		left_info.R.fill(0.0);
+		right_info.R.fill(0.0);
+		cv::Mat rvec = (cv::Mat_<double>(3, 1) << rx, ry, rz);
+		cv::Mat rmat(3, 3, CV_64F);
+		cv::Rodrigues(rvec, rmat);
+		int id = 0;
+		cv::MatIterator_<double> it, end;
+		for (it = rmat.begin<double>(); it != rmat.end<double>(); ++it, id++){
+			left_info.R[id] = *it;
+			right_info.R[id] = *it;
+		}
+
+		// Projection/camera matrix
+		//     [fx'  0  cx' Tx]
+		// P = [ 0  fy' cy' Ty]
+		//     [ 0   0   1   0]
+		left_info.P.fill(0.0);
+		left_info.P[0] = l_fx;
+		left_info.P[2] = l_cx;
+		left_info.P[5] = l_fy;
+		left_info.P[6] = l_cy;
+		left_info.P[10] = 1.0;
+
+		right_info.P.fill(0.0);
+		right_info.P[0] = r_fx;
+		right_info.P[2] = r_cx;
+		right_info.P[3] = (-1 * l_fx * baseline);
+		right_info.P[5] = r_fy;
+		right_info.P[6] = r_cy;
+		right_info.P[10] = 1.0;
+
+		left_info.width = right_info.width = width_;
+		left_info.height = right_info.height = height_;
+
+		left_info.header.frame_id = left_frame_id_;
+		right_info.header.frame_id = right_frame_id_;
+	}
+
+	/**
+	 * @brief      { publish cameara info }
+	 *
+	 * @param[in]  pub_cam_info  The pub camera information
+	 * @param[in]  cam_info_msg  The camera information message
+	 * @param[in]  now           The now
+	 */
+	void publishCamInfo(const ros::Publisher& pub_cam_info, sensor_msgs::CameraInfo& cam_info_msg, ros::Time now) {
+		cam_info_msg.header.stamp = now;
+		pub_cam_info.publish(cam_info_msg);
+	}
+
+	/**
+	 * @brief      { publish image }
+	 *
+	 * @param[in]  img           The image
+	 * @param      img_pub       The image pub
+	 * @param[in]  img_frame_id  The image frame identifier
+	 * @param[in]  t             { parameter_description }
+	 */
+	void publishImage(cv::Mat img, image_transport::Publisher &img_pub, std::string img_frame_id, ros::Time t) {
+		cv_bridge::CvImage cv_image;
+		cv_image.image = img;
+		cv_image.encoding = sensor_msgs::image_encodings::BGR8;
+		cv_image.header.frame_id = img_frame_id;
+		cv_image.header.stamp = t;
+		img_pub.publish(cv_image.toImageMsg());
+	}
+
+private:
+	int device_id_;	
+	int resolution_;
+	double frame_rate_;
+	bool show_image_, load_zed_config_;
+	double width_, height_;
+	std::string left_frame_id_, right_frame_id_;
+	std::string config_file_location_;
+};
+
+}
+
+
+int main(int argc, char **argv) {
+	ros::init(argc, argv, "zed_camera");
+	arti::ZedCameraROS zed_ros;
+	return 0;
+}
